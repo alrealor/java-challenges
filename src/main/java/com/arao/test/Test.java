55497fab493cbdbfad9b441ceda980385085301b
alrealor <isc.aortiz@gmail.com>
2022-06-22 11:21:21 -0500

    Strings - Excercise2 - Rever Integer

---
+++ src/main/java/com/arao/test/Test.java
@@ -0,0 +1,8 @@
+package com.arao.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.print(Integer.MIN_VALUE);
+    }
+}
